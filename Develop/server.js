acbfe9e6b69b95e7e2d88ae00beea2c7cb22b338
New Dad <josh_panakkal3122@hotmail.com>
2020-09-19 14:58:11 -0400

    add html routes to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const path = require("path");
+// const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000
 
@@ -10,16 +11,29 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useUnifiedTopology: true
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true
+// });
 
 // routes
-// app.use(require("./routes/api-routes.js"));
-// app.use(require("./routes/html-routes.js"));
+// WHY DOESN' THIS WORK????
+// require("./routes/html-routes.js")(app);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
