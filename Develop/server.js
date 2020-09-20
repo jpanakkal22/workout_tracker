e688fd2ba80089abbc99aaa1bc26e04cee99a993
New Dad <josh_panakkal3122@hotmail.com>
2020-09-20 14:05:13 -0400

    create /exercise and /stats routes which serve their html files

--- Develop/server.js
+++ Develop/server.js
@@ -11,11 +11,23 @@ const port = 3000
 // Serve static files
 app.use(express.static('public'));
 
+// HTML Routes
 // Create a root route and handler 
 app.get('/', (req, res) => {
   // Send message
-  res.sendFile(path.join(__dirname + 'index.html'));
-})
+  res.sendFile(path.join(`${__dirname}/public/index.html`));
+});
+// Create a root route and handler 
+app.get('/exercise', (req, res) => {
+  // Send message
+  res.sendFile(path.join(__dirname + '/public/exercise.html'));
+});
+// Create a root route and handler 
+app.get('/stats', (req, res) => {
+  // Send message
+  res.sendFile(path.join(__dirname + '/public/stats.html'));
+});
+
 // Listen on port 3000
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
