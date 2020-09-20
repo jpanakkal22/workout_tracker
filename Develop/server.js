3c1d9592a81694b79d9893e5d3f797fb1f657cb8
New Dad <josh_panakkal3122@hotmail.com>
2020-09-20 13:57:45 -0400

    require path and create route to serve index.html

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,20 @@
 // Require Express 
 const express = require("express");
+// Require path
+const path = require("path");
+
 // Create an instance of express
 const app = express();
 // Create a port
 const port = 3000
+
+// Serve static files
+app.use(express.static('public'));
+
 // Create a root route and handler 
 app.get('/', (req, res) => {
   // Send message
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname + 'index.html'));
 })
 // Listen on port 3000
 app.listen(port, () => {
