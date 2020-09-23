1bd5bc761bfdcc2bd05abec53d598ffae896af5c
New Dad <josh_panakkal3122@hotmail.com>
2020-09-23 16:28:33 -0400

    update mongoose connction

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,60 +1,60 @@
-// // Require Dependencies
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const path = require("path");
+// Require Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
-// // Require Models folder
-// const db = require("../models");
+// Require Models folder
+const db = require("../models");
 
-// module.exports = function(app) {    
+module.exports = function(app) {    
     
-//     // GET Route for all workouts
-//     app.get('/api/workouts', (req, res) => {
-//         db.Workout.find({}).then(data => {
-//         data.forEach(workout => {
-//             var total = 0;
-//             workout.exercises.forEach(e => {
-//                 total += e.duration;
-//             });
-//             workout.totalDuration = total;
-//         });
-//         res.json(data);
-//         }).catch(err => {
-//         res.json(err);
-//         });
-//     })
+    // GET Route for all workouts
+    app.get('/api/workouts', (req, res) => {
+        db.Workout.find({}).then(data => {
+        data.forEach(workout => {
+            var total = 0;
+            workout.exercises.forEach(e => {
+                total += e.duration;
+            });
+            workout.totalDuration = total;
+        });
+        res.json(data);
+        }).catch(err => {
+        res.json(err);
+        });
+    })
     
-//     // POST Route / posts an empty object with an id to be used for the update route
-//     app.post('/api/workouts', (req, res) => {  
-//         db.Workout.create({}).then((data => {
-//         res.json(data);
-//         console.log('Success!');
-//         })).catch(err => {
-//         res.json(err);
-//         });      
-//     });
+    // POST Route / posts an empty object with an id to be used for the update route
+    app.post('/api/workouts', (req, res) => {  
+        db.Workout.create({}).then((data => {
+        res.json(data);
+        console.log('Success!');
+        })).catch(err => {
+        res.json(err);
+        });      
+    });
     
-//     // Update Route for exercise page using id from post route
-//     app.put("/api/workouts/:id", (req, res) => {  
-//         db.Workout.findOneAndUpdate({_id: req.params.id}, 
-//         {
-//             $push: {exercises : req.body}
-//         },
-//         { new: true })    
-//             .then(dbWorkout => {
-//             return res.json(dbWorkout);
-//             }).catch(err => {
-//             res.json(err);
-//             });       
-//     });
+    // Update Route for exercise page using id from post route
+    app.put("/api/workouts/:id", (req, res) => {  
+        db.Workout.findOneAndUpdate({_id: req.params.id}, 
+        {
+            $push: {exercises : req.body}
+        },
+        { new: true })    
+            .then(dbWorkout => {
+            return res.json(dbWorkout);
+            }).catch(err => {
+            res.json(err);
+            });       
+    });
     
-//     // Get route for Fitness Tracker Dashboard
-//     app.get('/api/workouts/range', (req, res) => {
-//         db.Workout.find({}).then(summary => {
-//         res.json(summary);
-//         }).catch(err => {
-//         res.json(err);
-//         });
-//     });    
+    // Get route for Fitness Tracker Dashboard
+    app.get('/api/workouts/range', (req, res) => {
+        db.Workout.find({}).then(summary => {
+        res.json(summary);
+        }).catch(err => {
+        res.json(err);
+        });
+    });    
         
-// }
+}
--- server.js
+++ server.js
@@ -30,8 +30,8 @@ mongoose.connect(
 );
 
 
-// // Require api routes
-// require("./routes/api-routes.js")(app);
+// Require api routes
+require("./routes/api-routes.js")(app);
 
 // HTML Routes
 // Create a root route and handler 
@@ -49,55 +49,6 @@ app.get('/stats', (req, res) => {
   // Send message
   res.sendFile(path.join(__dirname + '/public/stats.html'));
 });
-
-// GET Route for all workouts
-app.get('/api/workouts', (req, res) => {
-  db.Workout.find({}).then(data => {
-  data.forEach(workout => {
-      var total = 0;
-      workout.exercises.forEach(e => {
-          total += e.duration;
-      });
-      workout.totalDuration = total;
-  });
-  res.json(data);
-  }).catch(err => {
-  res.json(err);
-  });
-})
-
-// POST Route / posts an empty object with an id to be used for the update route
-app.post('/api/workouts', (req, res) => {  
-  db.Workout.create({}).then((data => {
-  res.json(data);
-  console.log('Success!');
-  })).catch(err => {
-  res.json(err);
-  });      
-});
-
-// Update Route for exercise page using id from post route
-app.put("/api/workouts/:id", (req, res) => {  
-  db.Workout.findOneAndUpdate({_id: req.params.id}, 
-  {
-      $push: {exercises : req.body}
-  },
-  { new: true })    
-      .then(dbWorkout => {
-      return res.json(dbWorkout);
-      }).catch(err => {
-      res.json(err);
-      });       
-});
-
-// Get route for Fitness Tracker Dashboard
-app.get('/api/workouts/range', (req, res) => {
-  db.Workout.find({}).then(summary => {
-  res.json(summary);
-  }).catch(err => {
-  res.json(err);
-  });
-});    
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
